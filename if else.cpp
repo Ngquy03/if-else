c6cacb2108b529ac4729911f03ca414ab0e6daf3
Ngquy03 <nguyenquy29122003@gmail.com>
2023-08-19 00:15:41 +0700

    lon hon nho hon hoac bang khong

--- if else.cpp
+++ if else.cpp
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main(){
 	int a;
-	printf("3");
+	printf("nhap mot so:");
 	scanf("%d",&a);
 	if (a>0){
-		printf("%d lon hon khong\n",a);
+		printf("%d lon hon 0\n",a);
 	} else if(a<0){
-		printf("%d nho hon khong\n",a);
+		printf("%d nho hon 0\n",a);
 		
 	}else{
-		printf("%d==\n",a);
+		printf("%d bang 0\n",a);
 	}
 	return 0;
 }
